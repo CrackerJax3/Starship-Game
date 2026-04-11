1fa891c533ea3f26c22e206b065e410662608e92
Subasio Yuen <jax.yuen@gmail.com>
2026-04-11 00:43:54 -0400

    Revert "Fix Google Play Console warnings for Android 15 compatibility"
    This reverts commit c63374ff012269c3e17f24e9d6536714dccbcbe6.

--- android/app/src/main/java/com/crackerjax3/starshiplander/MainActivity.java
+++ android/app/src/main/java/com/crackerjax3/starshiplander/MainActivity.java
@@ -2,13 +2,11 @@ package com.crackerjax3.starshiplander;
 
 import android.os.Bundle;
 import android.view.View;
-import androidx.activity.EdgeToEdge;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        EdgeToEdge.enable(this);
         super.onCreate(savedInstanceState);
         // Disable overscroll rubber-band effect on the game WebView
         getBridge().getWebView().setOverScrollMode(View.OVER_SCROLL_NEVER);
